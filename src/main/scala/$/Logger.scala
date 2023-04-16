8df6c0dd693535c453f556644afbcfd57c4963d7
Isaac Tsang <ndtm.idea@gmail.com>
2023-04-17 00:32:54 +0800

    refactor

--- src/main/scala/$/Logger.scala
+++ src/main/scala/$/Logger.scala
@@ -2,10 +2,7 @@ package $
 
 
 trait Logger(name: String = null) {
-  val log = {
-    val lname = if (name != null) name else s"${this.getClass.getSimpleName}"
-    com.typesafe.scalalogging.Logger(lname)
-  }
+  val log = com.typesafe.scalalogging.Logger(name || this.getClass.getSimpleName)
 }
 
 val Log = com.typesafe.scalalogging.Logger("")
