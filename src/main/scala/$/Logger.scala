d67fc64a7f89965bdf5c5b2dac8f2e0f8edb54f2
Isaac Tsang <ndtm.idea@gmail.com>
2023-04-16 17:45:08 +0800

    quickfix

--- src/main/scala/$/Logger.scala
+++ src/main/scala/$/Logger.scala
@@ -4,7 +4,7 @@ package $
 trait Logger(name: String = null) {
   val log = {
     val lname = if (name != null) name else s"${this.getClass.getSimpleName}"
-    com.typesafe.scalalogging.Logger(name)
+    com.typesafe.scalalogging.Logger(lname)
   }
 }
 
