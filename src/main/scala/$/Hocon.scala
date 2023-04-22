78925ecd58a9b0457ddad4e3ecaa14daf819a3bb
Isaac Tsang <ndtm.idea@gmail.com>
2023-04-22 18:20:18 +0800

    Hocon use JavaTypeable instead of ClassTag

--- src/main/scala/$/Hocon.scala
+++ src/main/scala/$/Hocon.scala
@@ -4,19 +4,20 @@ import scala.reflect.{classTag, ClassTag}
 
 import java.io.File
 import com.typesafe.config.{Config, ConfigBeanFactory, ConfigFactory, ConfigRenderOptions}
+import com.fasterxml.jackson.module.scala.JavaTypeable
 
 object Hocon {
 
   export ConfigFactory.*
 
-  def read[T: ClassTag](c: Config): T = {
+  def read[T: JavaTypeable](c: Config): T = {
     JSON.read[T](c.root().render(ConfigRenderOptions.concise()))
   }
 
-  def read[T: ClassTag](f: File): T = read[T](parseFile(f))
+  def read[T: JavaTypeable](f: File): T = read[T](parseFile(f))
 
   extension (c: Config) {
-    def as[T: ClassTag] = read[T](c)
+    def as[T: JavaTypeable] = read[T](c)
   }
 
 }
